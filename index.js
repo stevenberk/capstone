47496f2415e438b53c06c0dc28d6fa02b9246817
Steven Berk <steven.e.berk@gmail.com>
2018-09-26 21:48:36 -0400

    drop down for amount added

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ let URL = 'http://data.fixer.io/api/latest?access_key=';
 let maindiv = document.querySelector(".apioutput");
 let submitButton = document.querySelector('.submitButton');
 let currenyLog = document.querySelector('.currencyList');
+let valueLog = document.querySelector('.valueList');
 
 
 
 submitButton.addEventListener("click", LogCunch);
 
 function LogCunch() {
-    if (currenyLog.value == "NULL"){
-        console.log('please select a currency');
+    if (currenyLog.value == "NULL" || valueLog.value == "NULL"){
+        console.log('please make a valid selection');
     }else{
         fetch(URL + APIKEY)
         .then(function(response) {return response.json();})
         .then(function(myJson) {
             let exRate = myJson.rates[currenyLog.value] ;
-            maindiv.textContent = exRate;
+            maindiv.textContent = exRate * valueLog.value;
            });
     }
 }
