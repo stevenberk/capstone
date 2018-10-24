5fb160afcbd7acd25bf5aca910fb4cf202bf7307
Steven Berk <steven.e.berk@gmail.com>
2018-10-24 11:52:15 -0400

    demo instructions added to about page

--- capstone-app/src/About.js
+++ capstone-app/src/About.js
@@ -21,6 +21,13 @@ render(){
             <h5 className="pageheader">2. Before your trip, browse and purchase foreign banknotes</h5>
             <h5 className="pageheader">3. After you return, post and sell your unused foreign currency to other travelers</h5>
         </div>
+        <h2 className="pageheader underliner">Try it out!</h2>
+        <div className="instructioncontainer">
+            <h5 className="pageheader">Don't want to make an account but still want to see how it works? Log in with the following credentials to demo the features</h5>
+            <h5 className="pageheader">Email: user@user.com</h5>
+            <h5 className="pageheader">Password: password</h5>
+            <h5 className="pageheader">There are dummy data submissions for Atlanta/CAD, Boston/EUR, New York/GBP</h5>
+        </div>
             <a href="https://github.com/stevenberk/Capstone">GitHub repo</a>
             <a href="https://stevenberk.github.io">Resume</a>
       
